69367e4ea6ad498866c23aaf617acdde9a2792a6
Angel Alegre <fisiangel.14@gmail.com>
2020-10-27 18:04:27 -0500

    operadores de asignacion

--- scripts.js
+++ scripts.js
@@ -3,6 +3,19 @@
  */
 
 // +  -  *  /  %
-let operacion = 62 %4
+// let operacion = 62 %4
 
-console.log(operacion)
+// console.log(operacion)
+
+/**
+ * OEREADORES DE ASIGNACION
+ */
+
+let a = 10 
+let b = 2
+
+// a = a + b
+a += b
+
+// Potencia
+console.log(a**b)
