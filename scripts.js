9f9455914daf418ca0c80a5bf766ed7c1cd4d2a0
Angel Alegre <fisiangel.14@gmail.com>
2020-10-27 17:59:22 -0500

    operadores matematicos

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
 /**
- * CONSTANTES
+ * OPERADORES ARITEMETICOS
  */
-// Debe ir en mayusculas
-// Se debe de incializar al incio
-// no se puede reasignar
 
-const NOMBRE_EMPRESA = "EDteam"
-const SITIO_WEB = "https://edteam.com"
-const PI = 3.14
+// +  -  *  /  %
+let operacion = 62 %4
+
+console.log(operacion)
