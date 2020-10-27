be49f9508bccc9207212b36ea966380e80ab26f7
Angel Alegre <fisiangel.14@gmail.com>
2020-10-27 11:22:11 -0500

    'comentarios'

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Comentario en una linea
+
+// Comentario multilinea
+/*
+Esto es un comentario de una linea
+Y en las siguientes lineas 
+tambien abra otros mas
+*/
+
+// Comentario mas estetico
+/**
+ * Esto es un
+ * Comentario con 
+ * asteristicos
+ */
+
+ // Para comentar bloques --> ctrl + }
+// var i = 0;
+// i++;
+// console.log(i);
