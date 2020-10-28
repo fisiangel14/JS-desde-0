e17e29b5c74e9c2d536cadd761d76ce3e4caac18
Angel Alegre <fisiangel.14@gmail.com>
2020-10-28 12:55:19 -0500

    operadors de concatenacion

--- scripts.js
+++ scripts.js
@@ -11,11 +11,26 @@
  * OEREADORES DE ASIGNACION
  */
 
-let a = 10 
-let b = 2
+// let a = 10 
+// let b = 2
 
-// a = a + b
-a += b
+// // a = a + b
+// a += b
 
-// Potencia
-console.log(a**b)
+// // Potencia
+// console.log(a**b)
+
+/**
+ * OPERADORES DE CONCATENACION
+ *          +
+ */
+
+let nombre = "Angel Eduardo"
+let apellido = "Alegre Quispe"
+
+let nombreCompleto = nombre + " " + apellido
+
+console.log(nombreCompleto)
+
+let nickname = `Mi nombre es: ${nombre} y mi apellido es ${apellido}`
+console.log(nickname)
