4cc6c7de0d0563bfda507b1537b54cfa60a12e92
Angel Alegre <fisiangel.14@gmail.com>
2020-10-29 00:07:15 -0500

    funcion con retorno

--- funciones.js
+++ funciones.js
@@ -3,15 +3,25 @@
  */
 
  // declaracion de la funcion
-function saludar(nombre) {
-    if (typeof nombre === "string") {
-        console.log(`Hola, ${nombre} muy buenos dias`)
-    }else{
-        console.log("Es incorrecto")
-    }
+// function saludar(nombre) {
+//     if (typeof nombre === "string") {
+//         console.log(`Hola, ${nombre} muy buenos dias`)
+//     }else{
+//         console.log("Es incorrecto")
+//     }
     
+// }
+
+
+// // llamada a la funcion saludar
+// saludar("Angel")
+
+/**FUNCION CON RETORNO
+ * 
+ */
+
+function getAllName(firstName, lastName) {
+    return `${firstName} ${lastName}`
 }
 
-
-// llamada a la funcion saludar
-saludar("Angel")
+console.log(getAllName("Angel Eduardo", "Alegre Quispe"))
