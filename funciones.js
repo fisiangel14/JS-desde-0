40b461f4323ac1438955d57c9ee3292481cb7215
Angel Alegre <fisiangel.14@gmail.com>
2020-10-28 23:57:19 -0500

    fucniones - parametros

--- funciones.js
+++ funciones.js
@@ -1,11 +1,17 @@
 /**
- * FUNCIONES
+ * FUNCIONES - parametros
  */
 
  // declaracion de la funcion
-function saludar() {
-    console.log("Hola, muy buenos dias")
+function saludar(nombre) {
+    if (typeof nombre === "string") {
+        console.log(`Hola, ${nombre} muy buenos dias`)
+    }else{
+        console.log("Es incorrecto")
+    }
+    
 }
 
+
 // llamada a la funcion saludar
-saludar()
+saludar("Angel")
