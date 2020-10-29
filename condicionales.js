21e4b390eaf9a14c6836a89b2220222a86f0a773
Angel Alegre <fisiangel.14@gmail.com>
2020-10-28 21:51:28 -0500

    condicional if else if

--- condicionales.js
+++ condicionales.js
@@ -4,10 +4,13 @@
  */
 
 let persona = "Angel Alegre"
-let edad = 16
+let edad = 12
+let permiso = true
 
 if (edad > 18 && edad < 65) {
     console.log(`${persona} puedes venir a la fiesta!!`)
-} else {
+} else if (permiso){
+    console.log(`Tanbien puedes venir`)
+}else{
     console.log(`Por favor quedate en casa ${persona}`)
 }
