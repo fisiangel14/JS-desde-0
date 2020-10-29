91234bee7ef9b7b7e97de4023627797dd2f44674
Angel Alegre <fisiangel.14@gmail.com>
2020-10-29 18:42:48 -0500

    array y metodos

---
+++ array.js
@@ -0,0 +1,13 @@
+/**
+ * ARRAY
+ */
+
+let amigos = ["Angel","Miguel","Fernando","Adriana","Luana"]
+
+console.log(amigos)
+
+//agregar elementos al array
+
+amigos.push("Alomso")
+
+console.log(amigos)
