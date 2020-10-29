b2edbc31ea36a1bba8469cf93764ce2b500b8896
Angel Alegre <fisiangel.14@gmail.com>
2020-10-28 22:04:49 -0500

    switch

--- condicionales.js
+++ condicionales.js
@@ -3,14 +3,36 @@
  * 
  */
 
-let persona = "Angel Alegre"
-let edad = 12
-let permiso = true
+// let persona = "Angel Alegre"
+// let edad = 12
+// let permiso = true
 
-if (edad > 18 && edad < 65) {
-    console.log(`${persona} puedes venir a la fiesta!!`)
-} else if (permiso){
-    console.log(`Tanbien puedes venir`)
-}else{
-    console.log(`Por favor quedate en casa ${persona}`)
+// if (edad > 18 && edad < 65) {
+//     console.log(`${persona} puedes venir a la fiesta!!`)
+// } else if (permiso){
+//     console.log(`Tanbien puedes venir`)
+// }else{
+//     console.log(`Por favor quedate en casa ${persona}`)
+// }
+
+
+/**
+ * CONDICIONALES MULTIPLES - SWITCH
+ */
+
+let op = 2
+
+switch (op) {
+    case 1:
+        console.log("Esta es una Suma")        
+        break;
+    case 2:
+        console.log("Esta es una Resta")
+        break
+    case 3:
+        console.log("esta es una multiplicacion")
+        break
+    default:
+        console.log("esta es una division")
+        break;
 }
