c955b60d0e9c5fa7768ee350a4069d80abb3759b
Angel Alegre <fisiangel.14@gmail.com>
2020-10-29 19:01:29 -0500

    array y metodos 2

--- array.js
+++ array.js
@@ -11,3 +11,11 @@ console.log(amigos)
 amigos.push("Alomso")
 
 console.log(amigos)
+
+//para quitar 1 elemento
+amigos.pop()
+console.log(amigos)
+
+//para partir el arrary en 2, se debe guardar en una variable
+let dato = amigos.slice(0,2)
+console.log(dato)
