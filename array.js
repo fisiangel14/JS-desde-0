3b6678d243ce27369ae691bc2a8d221caf61b933
Angel Alegre <fisiangel.14@gmail.com>
2020-10-29 22:14:36 -0500

    foreach

--- array.js
+++ array.js
@@ -2,20 +2,29 @@
  * ARRAY
  */
 
+// let amigos = ["Angel","Miguel","Fernando","Adriana","Luana"]
+
+// console.log(amigos)
+
+// //agregar elementos al array
+
+// amigos.push("Alomso")
+
+// console.log(amigos)
+
+// //para quitar 1 elemento
+// amigos.pop()
+// console.log(amigos)
+
+// //para partir el arrary en 2, se debe guardar en una variable
+// let dato = amigos.slice(0,2)
+// console.log(dato)
+
+/**
+ * foreach
+ */
+
 let amigos = ["Angel","Miguel","Fernando","Adriana","Luana"]
 
-console.log(amigos)
+amigos.forEach(amigo => console.log(amigo))
 
-//agregar elementos al array
-
-amigos.push("Alomso")
-
-console.log(amigos)
-
-//para quitar 1 elemento
-amigos.pop()
-console.log(amigos)
-
-//para partir el arrary en 2, se debe guardar en una variable
-let dato = amigos.slice(0,2)
-console.log(dato)
